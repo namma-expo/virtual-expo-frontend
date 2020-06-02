cf7799c5e74eb400d0bf6dddcfd140f076d4fb1d
Nagaraj <nagainsr@gmail.com>
2020-06-02 22:25:46 +0530

    Fix routing issue for dashboard

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import DashboardPage from './components/Dashboard';
+import HomePage from 'components/HomePage';
+import DashboardPage from 'components/Dashboard';
 
 export default function App() {
   return (
     <Router>
       <div>
         <Switch>
-          <Route path="/">
-            <HomePage />
-          </Route>
           <Route path="/dashboard">
             <DashboardPage />
+          </Route>
+
+          {/* Home page should always be at the bottom */}
+          <Route path="/">
+            <HomePage />
           </Route>
         </Switch>
       </div>
