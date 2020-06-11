fb53932a4d5e796ab2a834de658dcae7522f298f
Nagaraj <nagainsr@gmail.com>
2020-06-11 21:17:17 +0530

    Add basic chat support using firebase

--- src/components/ExhibitorDashboard/Chat/index.js
+++ src/components/ExhibitorDashboard/Chat/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ThemeProvider } from '@material-ui/styles';
 import { muiTheme } from 'common/Style/mui-theme';
 import { withStyles } from '@material-ui/core/styles';
+import { useForm } from 'react-hook-form';
 import {
   InputBase,
   IconButton,
@@ -12,6 +13,7 @@ import {
 } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import SendIcon from '@material-ui/icons/Send';
+import { useChatFeed, useChatRecents, sendMessage } from 'common/Chat/utils';
 import {
   useStyles,
   styledBadgeStyle,
@@ -34,127 +36,123 @@ import {
 
 const StyledBadge = withStyles(styledBadgeStyle)(Badge);
 
-export default function ExhibitorChatPage(props) {
+const ChatMessageCard = ({ userMessage = false, userText }) => {
+  return (
+    <ChatMesCard userMsg={userMessage}>
+      <ChatMsgBox userMsg={userMessage}>
+        <Typography variant="body1">{userText}</Typography>
+      </ChatMsgBox>
+      <Typography component="span" variant="body2">
+        4/22/20, 6:30 PM
+      </Typography>
+    </ChatMesCard>
+  );
+};
+
+const ChatRecentButton = ({ userName, userText }) => {
   const classes = useStyles();
   return (
-    <ThemeProvider theme={muiTheme}>
-      <ChatContainer>
-        <ChatContentWrapper>
-          <ChatContentSection>
-            <ChatContentCard>
-              <ChatLeftWrapper>
-                <SearchSection>
-                  <SearchPaper>
-                    <InputBase
-                      placeholder="Search Contact"
-                      inputProps={{ 'aria-label': 'search contact' }}
-                    />
-                    <IconButton type="submit" aria-label="search">
-                      <SearchIcon />
-                    </IconButton>
-                  </SearchPaper>
-                </SearchSection>
-                <ChatItemsWrapper>
-                  <Button className={classes.chatListBtn}>
-                    <StyledBadge
-                      badgeContent={4}
-                      className={classes.avatarStyle}
-                    >
-                      <Avatar>M</Avatar>
-                    </StyledBadge>
-                    <ChatItemText>
-                      <Typography component="h6" variant="body1">
-                        Madhukar
-                      </Typography>
-                      <Typography variant="body1">
-                        distracted by the readable content of a page when
-                        looking at its layout. The point
-                      </Typography>
-                    </ChatItemText>
-                  </Button>
-                  <Button className={classes.chatListBtn}>
-                    <StyledBadge
-                      badgeContent={8}
-                      className={classes.avatarStyle}
-                    >
-                      <Avatar>N</Avatar>
-                    </StyledBadge>
-                    <ChatItemText>
-                      <Typography component="h6" variant="body1">
-                        Nagraj
-                      </Typography>
-                      <Typography variant="body1">
-                        Lorem Ipsum is simply dummy text of the printing and
-                        typesetting industry. Lorem Ipsum has been
-                      </Typography>
-                    </ChatItemText>
-                  </Button>
-                  <Button className={classes.chatListBtn}>
-                    <StyledBadge
-                      badgeContent={2}
-                      className={classes.avatarStyle}
-                    >
-                      <Avatar>M</Avatar>
-                    </StyledBadge>
-                    <ChatItemText>
-                      <Typography component="h6" variant="body1">
-                        Manoj
-                      </Typography>
-                      <Typography variant="body1">
-                        Contrary to popular belief, Lorem Ipsum is not simply
-                        random text. It has roots in a piece of classical
-                      </Typography>
-                    </ChatItemText>
-                  </Button>
-                </ChatItemsWrapper>
-              </ChatLeftWrapper>
-              <ChatRightWrapper>
-                <ChatMsgHeader>
-                  <Avatar>M</Avatar>
-                  <Typography component="h6" variant="body1">
-                    Madhukar
-                  </Typography>
-                </ChatMsgHeader>
-                <ChatMsgsWrapper>
-                  <ChatMesCard>
-                    <ChatMsgBox>
-                      <Typography variant="body1">
-                        Contrary to popular belief, Lorem Ipsum is not simply
-                        random text. It has roots in a piece of classical
-                      </Typography>
-                    </ChatMsgBox>
-                    <Typography component="span" variant="body2">
-                      4/22/20, 6:30 PM
+    <Button className={classes.chatListBtn}>
+      <StyledBadge badgeContent={''} className={classes.avatarStyle}>
+        <Avatar>M</Avatar>
+      </StyledBadge>
+      <ChatItemText>
+        <Typography component="h6" variant="body1">
+          {userName}
+        </Typography>
+        <Typography variant="body1">{userText}</Typography>
+      </ChatItemText>
+    </Button>
+  );
+};
+
+export default function ExhibitorChatPage(props) {
+  const { register, handleSubmit, reset } = useForm();
+  // const exhibitorID = 'ex231';
+  // const visitorID = 'vis9999';
+
+  const exhibitorID = 'ex231';
+  const visitorID = 'vis9999';
+
+  const chatFeed = useChatFeed({
+    userID: exhibitorID,
+    recepientID: visitorID,
+  });
+
+  const chatRecents = useChatRecents(exhibitorID);
+
+  return (
+    <form
+      onSubmit={handleSubmit((data) => {
+        sendMessage({
+          message: data.userText,
+          userID: exhibitorID,
+          recepientID: visitorID,
+        });
+        reset();
+      })}
+    >
+      <ThemeProvider theme={muiTheme}>
+        <ChatContainer>
+          <ChatContentWrapper>
+            <ChatContentSection>
+              <ChatContentCard>
+                <ChatLeftWrapper>
+                  <SearchSection>
+                    <SearchPaper>
+                      <InputBase
+                        placeholder="Search Contact"
+                        inputProps={{ 'aria-label': 'search contact' }}
+                      />
+                      <IconButton type="submit" aria-label="search">
+                        <SearchIcon />
+                      </IconButton>
+                    </SearchPaper>
+                  </SearchSection>
+                  <ChatItemsWrapper>
+                    {chatRecents.map(({ message, recepientID }) => (
+                      <ChatRecentButton
+                        userName={recepientID}
+                        userText={message}
+                      />
+                    ))}
+                  </ChatItemsWrapper>
+                </ChatLeftWrapper>
+                <ChatRightWrapper>
+                  <ChatMsgHeader>
+                    <Avatar>M</Avatar>
+                    <Typography component="h6" variant="body1">
+                      {visitorID}
                     </Typography>
-                  </ChatMesCard>
-                  <ChatMesCard userMsg>
-                    <ChatMsgBox userMsg>
-                      <Typography variant="body1">
-                        Contrary to popular belief, Lorem Ipsum is not simply
-                        random text. It has roots in a piece of classical
-                      </Typography>
-                    </ChatMsgBox>
-                    <Typography component="span" variant="body2">
-                      4/22/20, 6:35 PM
-                    </Typography>
-                  </ChatMesCard>
-                </ChatMsgsWrapper>
-                <SendSectionCard>
-                  <SearchPaper>
-                    <InputBase
-                      placeholder="Type your message"
-                      inputProps={{ 'aria-label': 'Type your message' }}
-                    />
-                    <IconButton type="submit" aria-label="Send">
-                      <SendIcon />
-                    </IconButton>
-                  </SearchPaper>
-                </SendSectionCard>
-              </ChatRightWrapper>
-            </ChatContentCard>
-          </ChatContentSection>
-        </ChatContentWrapper>
-      </ChatContainer>
-    </ThemeProvider>
+                  </ChatMsgHeader>
+
+                  <ChatMsgsWrapper>
+                    {chatFeed.map(({ message, recepientID, userID }) => (
+                      <ChatMessageCard
+                        userMessage={userID === exhibitorID}
+                        userText={message}
+                      />
+                    ))}
+                  </ChatMsgsWrapper>
+                  <SendSectionCard>
+                    <SearchPaper>
+                      <InputBase
+                        name="userText"
+                        placeholder="Type your message"
+                        inputProps={{ 'aria-label': 'Type your message' }}
+                        inputRef={register}
+                      />
+                      <IconButton type="submit" aria-label="Send">
+                        <SendIcon />
+                      </IconButton>
+                    </SearchPaper>
+                  </SendSectionCard>
+                </ChatRightWrapper>
+              </ChatContentCard>
+            </ChatContentSection>
+          </ChatContentWrapper>
+        </ChatContainer>
+      </ThemeProvider>
+    </form>
   );
 }
