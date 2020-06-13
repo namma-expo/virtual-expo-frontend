3289ccda4440de1d40612297cf16af02617e96e0
madhukar <madhukarp92@gmail.com>
2020-06-13 15:44:54 +0530

    changed layout for theme dashboard

--- src/components/ExhibitorDashboard/Theme/style.js
+++ src/components/ExhibitorDashboard/Theme/style.js
@@ -1,5 +1,5 @@
 import { makeStyles } from '@material-ui/core/styles';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import {
   snBreakpoint,
   breakPointMuiSm,
@@ -9,6 +9,7 @@ import {
 export const ThemeContentWrapper = styled.div`
   width: 100%;
   padding: 30px 20px;
+
   @media (min-width: ${snBreakpoint(breakPointMuiSm)}) {
     padding: 30px 50px;
   }
@@ -17,44 +18,22 @@ export const ThemeContentWrapper = styled.div`
   }
 `;
 
-export const StyledCard = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: 120px auto;
-  grid-gap: 20px;
-  align-items: center;
-  padding: 15px 0px;
-  margin-bottom: 10px;
-`;
-
-export const StyledBox = styled.div`
-  text-align: right;
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  ${(props) =>
-    props.alignLeft &&
-    css`
-      text-align: left;
-      justify-content: flex-start;
-    `};
-  input {
-    min-width: 50px;
-    min-height: 25px;
-    padding: 14px 14px;
-  }
+export const TemplateFormWrapper = styled.div`
+  margin: 0px;
+  padding: 30px 20px 10px;
+  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
 `;
 
 export const useStyles = makeStyles((theme) => ({
-  rootPaperStyle: {
-    padding: '20px 4px',
-    display: 'flex',
-    flexDirection: 'column',
-    minWidth: '650px',
-  },
-  actionBtn: {
-    width: '200px',
-    padding: '12px 15px',
-    margin: '10px 70px',
+  colorFieldStyle: {
+    maxWidth: '100px',
+    '& div': {
+      padding: '0px',
+    },
+    '& input, textarea': {
+      padding: '14px',
+      fontSize: '15px',
+    },
   },
 }));
