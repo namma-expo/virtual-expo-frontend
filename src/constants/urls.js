ba57492b21c0a883d9ef428f7eaa50903172e4d0
Nagaraj <nagainsr@gmail.com>
2020-06-21 20:19:20 +0530

    Initial contacts fetch and add setup

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -4,3 +4,8 @@ export const AUTHENTICATION_URLS = {
   LOGIN: `${BASE_URL}/authenticate`,
   SIGNUP: `${BASE_URL}/signup`,
 };
+
+export const CONTACTS_URLS = {
+  CONTACTS: `${BASE_URL}/contact`,
+  ALL_CONTACTS: `${BASE_URL}/contacts`,
+};
